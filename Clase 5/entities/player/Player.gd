2abd31f9d8044ee82e80094d1876ec0c8f82b3c3
Gabriel Pasquale <pasqualeunq@gmail.com>
2021-05-08 13:54:50 -0300

    clase 5 player jump

--- Clase 5/entities/player/Player.gd
+++ Clase 5/entities/player/Player.gd
@@ -19,7 +19,6 @@ var projectile_container
 
 var velocity:Vector2 = Vector2.ZERO
 var snap_vector:Vector2 = SNAP_DIRECTION * SNAP_LENGHT
-var is_looking_to_the_right:int = 1
 
 func initialize(projectile_container):
 	self.projectile_container = projectile_container
@@ -45,17 +44,18 @@ func get_input():
 	else:
 		velocity.x = lerp(velocity.x, 0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
 
-	_set_animation(h_movement_direction)
+	_set_animation(h_movement_direction, jump)
 
 	var mouse_position:Vector2 = get_global_mouse_position()
 	cannon.look_at(mouse_position)
 	
-func _set_animation(h_movement_direction):
-	if !animation_player.is_playing() and h_movement_direction != 0:
+func _set_animation(h_movement_direction, jump):
+	if !animation_player.is_playing() and h_movement_direction != 0 and is_on_floor():
 		_play_animation("walk")
-	if h_movement_direction != 0 and is_looking_to_the_right != h_movement_direction:
-		is_looking_to_the_right = h_movement_direction
+	if h_movement_direction != 0 and int(!body.flip_h) != h_movement_direction:
 		_flip(h_movement_direction < 0)
+	if jump and is_on_floor():
+		_play_animation("jump")
 
 func _physics_process(delta):
 	get_input()
